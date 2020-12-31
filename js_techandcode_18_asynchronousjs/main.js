ff51bc96587894b88ba390c5021450a3d7bd4ffe
geopasx2020 <gkaramousalidis@hotmail.gr>
2020-12-31 16:51:54 +0200

    video 18 commit

---
+++ js_techandcode_18_asynchronousjs/main.js
@@ -0,0 +1,20 @@
+/*var a=(function(){
+    return 1;
+})();
+
+
+setTimeout(function(){a++},2000)
+
+
+console.log(a)*/
+const weatherDiv=document.querySelector('#weather')
+let weatherResponse="Παρακαλώ περιμένετε...."
+weatherDiv.innerText=weatherResponse
+
+function getWeather(){
+    return "Σήμερα θα έχει λιακάδα"
+}
+
+setTimeout(()=>{weatherResponse=getWeather()},3000);//emfanizetai afto poy exei mikrotero time
+setTimeout(()=>{weatherDiv.innerText=weatherResponse},6000);
+weatherDiv.innerText=weatherResponse
